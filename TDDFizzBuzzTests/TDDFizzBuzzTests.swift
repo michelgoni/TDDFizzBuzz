6bf4848326a44c72c621cca012ef2d970db4de64
Michel Goñi <michelgoni@MacBook-Pro-de-Michel.local>
2021-07-22 17:54:31 +0200

    testing that default number is printed

--- TDDFizzBuzzTests/TDDFizzBuzzTests.swift
+++ TDDFizzBuzzTests/TDDFizzBuzzTests.swift
@@ -8,3 +8,18 @@
 import XCTest
 @testable import TDDFizzBuzz
 
+final class FizzBuzzFactory {
+    
+    func retrieveResult(_ number: Int) -> String {
+        "\(number)"
+    }
+}
+
+class TDDFizzBuzzTests: XCTestCase {
+
+    func test_printsDefaultNumber() {
+        let sut = FizzBuzzFactory()
+        let result = sut.retrieveResult(1)
+        XCTAssertEqual("1", result)
+    }
+}
