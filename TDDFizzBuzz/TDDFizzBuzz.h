7840468421c88a8b1923a6929c33433c6250f806
Michel Goñi <michelgoni@MacBook-Pro-de-Michel.local>
2021-07-22 17:51:06 +0200

    initial commit

---
+++ TDDFizzBuzz/TDDFizzBuzz.h
@@ -0,0 +1,18 @@
+//
+//  TDDFizzBuzz.h
+//  TDDFizzBuzz
+//
+//  Created by Michel Goñi on 22/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TDDFizzBuzz.
+FOUNDATION_EXPORT double TDDFizzBuzzVersionNumber;
+
+//! Project version string for TDDFizzBuzz.
+FOUNDATION_EXPORT const unsigned char TDDFizzBuzzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDDFizzBuzz/PublicHeader.h>
+
+
---
+++ TDDFizzBuzzTests/TDDFizzBuzzTests.swift
@@ -0,0 +1,10 @@
+//
+//  TDDFizzBuzzTests.swift
+//  TDDFizzBuzzTests
+//
+//  Created by Michel Goñi on 22/7/21.
+//
+
+import XCTest
+@testable import TDDFizzBuzz
+
