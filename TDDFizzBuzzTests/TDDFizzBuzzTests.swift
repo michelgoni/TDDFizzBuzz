3a5355523b9f9b63005ad94f6bf69146c33fae20
Michel Goñi <michelgoni@MacBook-Pro-de-Michel.local>
2021-07-22 18:03:00 +0200

    Added helper method to make expectation clearer

--- TDDFizzBuzzTests/TDDFizzBuzzTests.swift
+++ TDDFizzBuzzTests/TDDFizzBuzzTests.swift
@@ -17,9 +17,21 @@ final class FizzBuzzFactory {
 
 class TDDFizzBuzzTests: XCTestCase {
 
-    func test_printsDefaultNumber() {
-        let sut = FizzBuzzFactory()
-        let result = sut.retrieveResult(1)
-        XCTAssertEqual("1", result)
+    func test_printsNonFizzBuzzNumbers() {
+        expect(prints: 1, withResult: "1")
+        expect(prints: 2, withResult: "2")
+    }
+  
+    
+    //MARK: -- Private implementations
+    private func makeSut() -> FizzBuzzFactory {
+        FizzBuzzFactory()
+    }
+    
+    private func expect(prints number: Int, withResult result: String) {
+        let sut = makeSut()
+        let result = sut.retrieveResult(number)
+        XCTAssertEqual(result, result)
     }
 }
+
